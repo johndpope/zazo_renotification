cf23ba7ed84070af9f82db68b4b2f32933af89b2
Ivan Kornilov <vano468@gmail.com>
2015-07-03 16:38:19 +0300

    Set programs#show styles

--- app/concepts/program/cell.rb
+++ app/concepts/program/cell.rb
@@ -3,6 +3,13 @@ class Program::Cell < Cell::Concept
   include ActionView::Helpers::FormHelper
   include ActionView::Helpers::FormOptionsHelper
   include SimpleForm::ActionViewExtensions::FormHelper
+  include ActionView::Helpers::CaptureHelper
+
+  property :name
+
+  def show
+    render :show
+  end
 
   def new
     render :new
@@ -10,5 +17,27 @@ class Program::Cell < Cell::Concept
 
   def edit
     render :edit
+  end
+
+  private
+
+  def status
+    model.setting.started ? '[active]' : '[inactive]'
+  end
+
+  def queries
+    join_relations :queries
+  end
+
+  def conditions
+    join_relations :conditions
+  end
+
+  def join_relations(key)
+    joined = model.send(key).pluck(:type).map do |type|
+      type.split('::').last
+    end.join(', ')
+    prefix = content_tag :b, key.to_s.capitalize
+    "#{prefix}: [#{joined}]"
   end
 end
