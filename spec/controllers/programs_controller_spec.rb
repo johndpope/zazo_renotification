58afe43cb9492f6bb6cc0de5de12252448ce3771
Ivan Kornilov <vano468@gmail.com>
2015-07-05 13:43:04 +0300

    Add basic programs controller specs

---
+++ spec/controllers/programs_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe ProgramsController, type: :controller do
+  let!(:program) { FactoryGirl.create :program }
+
+  render_views
+
+  describe 'GET #index' do
+    before { get :index }
+    it_behaves_like 'response status'
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+    it_behaves_like 'response status'
+  end
+
+  describe 'GET #edit' do
+    before { get :edit, id: program }
+    it_behaves_like 'response status'
+  end
+end
