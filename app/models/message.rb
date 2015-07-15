506cd85423ae290c05bf605a942dc1467393cb4f
Ivan Kornilov <vano468@gmail.com>
2015-07-15 12:57:18 +0300

    Set allowed statuses for message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,11 @@
 class Message < ActiveRecord::Base
+  ALLOWED_STATUSES = %w{sent canceled}
+
   belongs_to :program
   belongs_to :delayed_template
 
   validates :target, :body, :send_at, :program, :delayed_template, presence: true
+  validates :status, inclusion: { in: ALLOWED_STATUSES, message: "%{value} is not a valid status" }, allow_nil: true
 
   scope :in_progress_by_target, -> (target) { where(target: target).where(status: nil) }
   scope :time_passed, -> { where(status: nil).where('send_at < ?', Time.now) }
