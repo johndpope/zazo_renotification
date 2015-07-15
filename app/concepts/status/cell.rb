77f4258a369c41a9e5298dd8c8394a7e7e12c893
Ivan Kornilov <vano468@gmail.com>
2015-07-15 12:01:37 +0300

    Add title for every page

---
+++ app/concepts/status/cell.rb
@@ -0,0 +1,5 @@
+class Status::Cell < Cell::Concept
+  def show
+    render
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   resources :queries, only: [:index, :show]
   resources :templates, except: :show
+
   resources :programs, except: :show do
     get :options, :users, on: :member
     resources :delayed_templates, only: [:index, :create, :destroy] do
       get :sms, :email, :ios, :android, on: :collection
     end
   end
+
   resources :settings, only: [:update] do
     patch :queries, :conditions, on: :collection
   end
-  root to: 'programs#index'
+
+  root to: 'dashboard#index'
 end
