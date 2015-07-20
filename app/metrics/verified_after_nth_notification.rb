1f352b97301e2552d804eb715b28810417ae105e
Ivan Kornilov <vano468@gmail.com>
2015-07-20 17:41:03 +0300

    Get data for `VerifiedAfterNthNotification` metric

--- app/metrics/verified_after_nth_notification.rb
+++ app/metrics/verified_after_nth_notification.rb
@@ -4,20 +4,24 @@ class Metric::VerifiedAfterNthNotification < Metric::Base
   end
 
   def execute
-
+    EventsApi.new(users_data: users_data).fetch :verified_after_nth_notification
   end
 
   private
 
-  def data
+  def users_data
     messages = Message.order(:send_at).pluck(:target, :send_at)
-    messages.each_with_object({}) do |message, memo|
+    data = messages.each_with_object({}) do |message, memo|
       memo[message[0]] ||= []
       memo[message[0]] << message[1]
     end
-  end
-
-  def results
-
+    data.keys.each_with_object([]) do |user, memo|
+      data[user].each_with_index do |message, index|
+        next_sent_at = data[user][index + 1]
+        next_sent_at = Time.now + 10.years if next_sent_at.nil?
+        memo << { user_id: user,    msg_order: index + 1,
+                  sent_at: message, next_sent_at: next_sent_at }
+      end
+    end
   end
 end
--- lib/events_api.rb
+++ lib/events_api.rb
@@ -2,5 +2,6 @@ class EventsApi < BaseApi
   version  1
   base_uri Figaro.env.events_api_base_url
 
-  mapper default: { action: :get, prefix: 'metrics/filter' }
+  mapper default: { action: :get, prefix: 'metrics/filter' },
+         verified_after_nth_notification: { action: :post, prefix: 'metrics' } # TODO: refactor this using method missing
 end
