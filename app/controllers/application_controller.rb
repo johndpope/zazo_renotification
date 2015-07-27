3faf05efc102fb5ea283e8850408eba35da7f935
Ivan Kornilov <vano468@gmail.com>
2015-07-27 13:16:16 +0300

    Use http basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  http_basic_authenticate_with name:     Figaro.env.http_basic_username,
+                               password: Figaro.env.http_basic_password
 end
