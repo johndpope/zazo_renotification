b7081446df708cffbbe5e17abc4b1ee88fa44d31
Ivan Kornilov <vano468@gmail.com>
2015-08-03 12:13:53 +0300

    Implement `WriteLog` - wrapper of `Rails.logger`, log errors from notification api when sms sending

--- app/services/message/worker.rb
+++ app/services/message/worker.rb
@@ -1,7 +1,7 @@
 class Message::Worker
   def self.execute
     messages = Message.time_passed
-    Rails.logger.tagged('Message::Worker') { Rails.logger.debug "Was executed at #{Time.now} with #{messages.size} message(s)." }
+    WriteLog.debug self, "Was executed at #{Time.now} with #{messages.size} message(s)."
     messages.each { |message| Message::Send.new(message).do }
   end
 end
--- lib/notification_api.rb
+++ lib/notification_api.rb
@@ -3,6 +3,8 @@ class NotificationApi < BaseApi
   base_uri   Figaro.env.notification_api_base_url
   auth_token Figaro.env.notification_api_token
 
+  raise_errors false
+
   mapper sms:     { action: :post, prefix: 'notifications/sms' },
          default: { action: :get,  prefix: '' }
 end
