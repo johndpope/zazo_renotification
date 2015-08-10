09186a3b91725dad0dc06a3a9cb9074461b6af4a
Ivan Kornilov <vano468@gmail.com>
2015-08-10 19:12:03 +0300

    Add rollbar logging

--- lib/write_log.rb
+++ lib/write_log.rb
@@ -2,5 +2,6 @@ class WriteLog
   def self.debug(context, message)
     klass_name = context.instance_of?(Class) ? context.name : context.class.name
     Rails.logger.tagged(klass_name) { Rails.logger.info message }
+    Rollbar.info klass_name, message: message
   end
 end
