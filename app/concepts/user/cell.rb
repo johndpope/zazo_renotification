b612c6d11b2eb33f8fc77e5e9062cccedb8ec6ec
Ivan Kornilov <vano468@gmail.com>
2015-08-14 18:29:13 +0300

    Add styles for user messages

--- app/concepts/user/cell.rb
+++ app/concepts/user/cell.rb
@@ -1,5 +1,5 @@
 class User::Cell < Cell::Concept
-  property :id, :mkey, :status, :first_name, :last_name,
+  property :mkey, :status, :first_name, :last_name,
            :email, :mobile_number, :device_platform,
            :inviter, :invited_at, :messages, :conditions
 
@@ -9,11 +9,11 @@ class User::Cell < Cell::Concept
 
   private
 
-  def user_id
-    link_to id, "#{Figaro.env.statistics_api_base_url}/users/#{id}"
+  def id
+    link_to model.id, "#{Figaro.env.statistics_api_base_url}/users/#{model.id}"
   end
 
   def attributes
-    %i(user_id mkey status first_name last_name email mobile_number device_platform inviter invited_at)
+    %i(id mkey status first_name last_name email mobile_number device_platform inviter invited_at)
   end
 end
