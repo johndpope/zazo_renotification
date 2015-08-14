ceb29e7ca33b021c77dfbe1ab7a7b53260a3717e
Ivan Kornilov <vano468@gmail.com>
2015-08-14 18:59:54 +0300

    Refactor user concept

--- app/concepts/user/cell.rb
+++ app/concepts/user/cell.rb
@@ -1,19 +1,46 @@
 class User::Cell < Cell::Concept
-  property :mkey, :status, :first_name, :last_name,
-           :email, :mobile_number, :device_platform,
-           :inviter, :invited_at, :messages, :conditions
+  class MessagesTable < Cell::Concept
+    property :messages
+
+    def show
+      render
+    end
+  end
+
+  class ConditionsTable < Cell::Concept
+    property :conditions
+
+    def show
+      render
+    end
+  end
+
+  class AttributesTable < Cell::Concept
+    ATTRIBUTES = [
+      :id, :mkey, :status, :first_name, :last_name, :email,
+      :mobile_number, :device_platform, :inviter, :invited_at
+    ]
+
+    def show
+      render
+    end
+
+    private
+
+    def id
+      link_to model.id, "#{Figaro.env.statistics_api_base_url}/users/#{model.id}"
+    end
+
+    def value(attr)
+      if respond_to? attr, true
+        send attr
+      else
+        model.send attr
+      end
+    end
+  end
 
   def show
     render
-  end
-
-  private
-
-  def id
-    link_to model.id, "#{Figaro.env.statistics_api_base_url}/users/#{model.id}"
-  end
-
-  def attributes
-    %i(id mkey status first_name last_name email mobile_number device_platform inviter invited_at)
   end
 end
