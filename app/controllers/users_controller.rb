c8dbee8983a7eb7a7a9efab7d7d859688f9d01f8
Ivan Kornilov <vano468@gmail.com>
2015-08-14 16:11:11 +0300

    Implement user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
   def show
+  end
+
+  private
+
+  def set_user
+    @user = User.new params[:id]
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,40 @@
 class User
-  def initialize(user_id)
-    
+  attr_reader :id, :mkey, :status, :first_name, :last_name,
+              :email, :mobile_number, :device_platform,
+              :inviter, :invited_at, :messages, :conditions
+
+  def initialize(mkey)
+    @mkey = mkey
+    set_attrs
+    set_inviter
+    set_messages
+    set_conditions
+  end
+
+  private
+
+  def set_attrs
+    attrs  = %i{id status first_name last_name email mobile_number device_platform}
+    values = StatisticsApi.new(user: mkey, attrs: attrs).attributes
+    attrs.each do |attr|
+      instance_variable_set "@#{attr}".to_sym, values[attr.to_s]
+    end
+  end
+
+  def set_inviter
+    data = StatisticsApi.new(users: [mkey]).filter(:specific_users)[0]
+    @inviter    = data['inviter']
+    @invited_at = data['time_zero']
+  end
+
+  def set_messages
+    @messages = Message.where(target: mkey).order(:id)
+  end
+
+  def set_conditions
+    @conditions = Condition.nested.map do |cond|
+      { name:   cond.name,
+        result: cond.new.check(mkey) }
+    end
   end
 end
